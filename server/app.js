7ae631dae5b1f899b8751967a2dc16f2e90ca79d
tinabeattysmith <tinatest2020@gmail.com>
2020-12-05 22:42:09 -0500

    reset routes in app.js

--- server/app.js
+++ server/app.js
@@ -1,25 +1,33 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const sequelize = require('./db');
-const { User, PantryItem, Request, Category, Meal, Role } = require('./controllers/controllerIndex');
+const sequelize = require("./db");
+const {
+  User,
+  PantryItem,
+  Request,
+  Category,
+  Meal,
+  Role,
+} = require("./controllers/controllerIndex");
 
 sequelize.sync(); //pass in {force: true} to reset tables
 
 app.use(express.json());
 
 // Non-protected route
-app.use('/user', User);
+app.use("/user", User);
+// app.use('/item', PantryItem);
+// app.use('/meal', Meal);
 
 // Protected route
-app.use('/item', require('./middleware/validate-session'), PantryItem);
-app.use('/request', require('./middleware/validate-session'), Request);
-app.use('/category', require('./middleware/validate-session'), Category);
-app.use('/request', require('./middleware/validate-session'), Request);
-app.use('/meal', require('./middleware/validate-session'), Meal);
-app.use('/role', require('./middleware/validate-session'), Role);
+app.use("/item", require("./middleware/validate-session"), PantryItem);
+app.use("/request", require("./middleware/validate-session"), Request);
+app.use("/category", require("./middleware/validate-session"), Category);
+app.use("/request", require("./middleware/validate-session"), Request);
+app.use("/meal", require("./middleware/validate-session"), Meal);
+app.use("/role", require("./middleware/validate-session"), Role);
 
-
-app.listen(process.env.PORT, function() {
-    console.log(`App is listening on port ${process.env.PORT}.`)
+app.listen(process.env.PORT, function () {
+  console.log(`App is listening on port ${process.env.PORT}.`);
 });
